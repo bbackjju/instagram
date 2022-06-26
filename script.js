89c37582bdb89694a17c5b4f079cd2bf50d6079b
bbackjju <iiiamst16@gmail.com>
2022-06-26 19:22:25 +0900

    finish JavaScript

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
 document.addEventListener("DOMContentLoaded",
     function(e){
-        document.addEventListener("keydown",
+        let configID = document.querySelector("#idname i")
+        let idText = document.querySelector("#idname span")
+
+        configID.addEventListener("click",
             function(e){
-                if(e.key === "q"){
-                    alert("this is Q!")
+                idText.textContent = prompt("새로운 아이디를 입력하세요")
+            }
+        )
+
+        let profileEditButton = document.querySelector(".profile_info button")
+
+        let userInfo = document.querySelector("#userInfo")
+        let summary = document.querySelector("#summary")
+        let profileDetail = document.querySelector("#profileDetail")
+        let changing = false
+
+        profileEditButton.addEventListener("click",
+            function(e){
+                if(changing){
+                    let _userInfo = userInfo.querySelector("input").value
+                    let _summary = summary.querySelector("input").value
+                    let _profileDetail = profileDetail.querySelector("input").value
+
+                    userInfo.innerHTML = _userInfo
+                    summary.innerHTML = _summary
+
+                    if(_profileDetail.startsWith("http")){
+                        _profileDetail = "<a href="+ _profileDetail + ">"+ _profileDetail + "</a>"
+                    }
+
+                    profileDetail.innerHTML = _profileDetail
+
+                    e.target.textContent = "프로필 편집"
+                    changing = false
+                }else{
+                    let _userInfo = userInfo.textContent
+                    let _summary = summary.textContent
+                    let _profileDetail = profileDetail.textContent
+
+                    userInfo.innerHTML = "<input value=" + _userInfo + "></input>"
+                    summary.innerHTML = "<input value=" + _summary + "></input>"
+                    profileDetail.innerHTML = "<input value=" + _profileDetail + "></input>"
+                    
+                    e.target.textContent = "프로필 편집 완료"
+                    changing = true
                 }
             }
         )
-
-        let keydown = document.querySelector("#keydown")
-        keydown.addEventListener("keydown",
+        let profile_pic = document.querySelector("#profile_pic .circle_pic")
+        profile_pic.addEventListener("mouseover",
             function(e){
-                console.log(e.target.value)
+                e.target.style.filter = "grayscale(50%)"
             }
         )
         
-        let change = document.querySelector("#change")
-        change.addEventListener("change",
+        profile_pic.addEventListener("mouseleave",
             function(e){
-                console.log(e.target.value)
+                e.target.style.filter = "grayscale(0%)"
             }
         )
         
-        
-        let input = document.querySelector("#input")
-        input.addEventListener("input",
+        profile_pic.addEventListener("click",
             function(e){
-                console.log(e.target.value)
-            }
-        )
-
-        let range = document.querySelector("#range")
-        range.addEventListener("change",
-            function(e){
-                console.log("change!")
+                profile_pic.setAttribute("src",prompt("이미지 url을 입력해주세요!"))
             })
-    }
-)
+
+    })
